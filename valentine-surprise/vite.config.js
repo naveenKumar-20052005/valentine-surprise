cdad49c4ffdd56dd6966837f828606a95a44a53a
naveenKumar-20052005 <naveenkumar20052005@gmail.com>
2026-02-14 15:21:30 +0530

    Valentine surprise for Kuttyma

---
+++ valentine-surprise/vite.config.js
@@ -0,0 +1,48 @@
+// // import { defineConfig } from 'vite'
+// // import react from '@vitejs/plugin-react'
+
+// // // https://vite.dev/config/
+// // export default defineConfig({
+// //   plugins: [react()],
+// // })
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// export default defineConfig({
+//   plugins: [react()],
+//   base: './',
+//   build: {
+//     outDir: 'dist',
+//     minify: 'terser',
+//     sourcemap: false,
+//     rollupOptions: {
+//       output: {
+//         manualChunks: undefined
+//       }
+//     }
+//   },
+//   server: {
+//     port: 3000,
+//     open: true
+//   }
+// })
+
+
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// export default defineConfig({
+//   plugins: [react()],
+//   base: '/valentine-surprise/',
+// })
+
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/valentine-surprise/',
+})
